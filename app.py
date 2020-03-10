4eca8fb48c8f0b4ddd47a022b73da88f93ec3a42
youssef34561 <61998204+youssef34561@users.noreply.github.com>
2020-03-10 18:29:37 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
-from utilities import *
-import cv2
-import base64
-from io import BytesIO
-from PIL import Image
-import face_recognition
-import cv2
-from gtts import gTTS
 import numpy as np
-import pymongo
 import json
+import base64
+import skimage.io
 
 app = Flask(__name__)
 
+def decode(base64_string):
+    if isinstance(base64_string, bytes):
+        base64_string = base64_string.decode("utf-8")
+
+    imgdata = base64.b64decode(base64_string)
+    img = skimage.io.imread(imgdata, plugin='imageio')
+    return np.array(img)
 
 @app.route("/")
 def index():
@@ -22,23 +22,12 @@ def index():
 
 @app.route("/photo", methods=["POST"])
 def index2():
-    read = json.loads(request.get_json())
+    read = request.get_json()
+    if type(read) == str:
+        read = json.loads(read)
     img = read['img']
-    img = decode(img)
-
-    save = read['save']
-    if save:
-        save_face(img, read['name'])
-        return "", 204
-    else:
-        new_img, names = get_faces(img)
-        sound_bytes = read_names(names)
-        cv2.imwrite('img.jpg', new_img)
-
-        new_img_encoded = encode(new_img)
-        data = {}
-        data['img'] = str(new_img_encoded)
-        data['sound'] = str(sound_bytes)
-        data['names'] = names
-
-        return jsonify(data)
+    img2 = decode(img)
+    data = {}
+    data['shape'] = img2.shape
+    
+    return jsonify(data)
